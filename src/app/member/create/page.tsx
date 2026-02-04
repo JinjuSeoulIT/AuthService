93212fe8a710efda4ab5d58137a0be668b385927
JOUNGCHON <jch6957@gmail.com>
2026-02-04 11:57:53 +0900

    Delete src directory

--- src/app/member/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import MemberCreate from "@/components/member/MemberCreate";
-
-export default function SignupPage() {
-  return <MemberCreate />;
-}
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/page.tsx
-import HomePage from "../components/home/HomePage";
-
-export default function Page() {
-  return <HomePage />;
-}
